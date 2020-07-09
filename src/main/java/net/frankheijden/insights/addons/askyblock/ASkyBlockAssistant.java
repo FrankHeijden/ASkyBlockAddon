a065f002b21507080d651e090eb1481a0cd5aef6
Frank van der Heijden <frank.boekanier@gmail.com>
2020-07-09 20:48:16 +0200

    Initial commit

---
+++ src/main/java/net/frankheijden/insights/addons/askyblock/ASkyBlockAssistant.java
@@ -0,0 +1,45 @@
+package net.frankheijden.insights.addons.askyblock;
+
+import com.wasteofplastic.askyblock.ASkyBlockAPI;
+import com.wasteofplastic.askyblock.Island;
+import com.wasteofplastic.askyblock.Settings;
+import net.frankheijden.insights.entities.CacheAssistant;
+import net.frankheijden.insights.entities.Selection;
+import org.bukkit.Location;
+import org.bukkit.World;
+
+public class ASkyBlockAssistant extends CacheAssistant {
+
+    public ASkyBlockAssistant() {
+        super("ASkyBlock", "ASkyBlock", "island", "1.0.0");
+    }
+
+    public Selection adapt(Island island) {
+        if (island == null) return null;
+        return new Selection(getMin(island), getMax(island));
+    }
+
+    public Location getMin(Island island) {
+        return new Location(
+                island.getCenter().getWorld(),
+                island.getMinProtectedX(),
+                0,
+                island.getMinProtectedZ()
+        );
+    }
+
+    public Location getMax(Island island) {
+        World world = island.getCenter().getWorld();
+        return new Location(
+                world,
+                island.getMinProtectedX() + Settings.islandProtectionRange,
+                world.getMaxHeight() - 1,
+                island.getMinProtectedZ() + Settings.islandProtectionRange
+        );
+    }
+
+    @Override
+    public Selection getSelection(Location location) {
+        return adapt(ASkyBlockAPI.getInstance().getIslandAt(location));
+    }
+}
